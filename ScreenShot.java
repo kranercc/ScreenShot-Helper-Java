efec811c16089ce07599ab6ae7bdebd5014c3302
kranewasd <kranehecaru@gmail.com>
2019-09-05 13:12:24 +0300

    Update ScreenShot.java

--- ScreenShot.java
+++ ScreenShot.java
@@ -3,8 +3,12 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -38,9 +42,18 @@ public class ScreenShot extends JFrame
 		
 	}
 	
-	public void createRegion(int w, int h, int x, int y)
+	public void createRegion(int x, int y, int w, int h)
 	{
 		this.rect = new Rectangle(x, y, w, h);
+		/*System.out.println(
+				
+				"X: " + this.rect.getX() +
+				"\nY: " + this.rect.getY() +
+				"\nWidth: " + this.rect.getWidth() +
+				"\nHeight: " + this.rect.getHeight()
+				
+				);
+		*/
 		
 	}
 	
@@ -49,10 +62,36 @@ public class ScreenShot extends JFrame
 		this.im = r.createScreenCapture(this.rect);
 	}
 	
-	public void saveSS() throws IOException
+	public void saveSS()
 	{
-		ImageIO.write(this.im, "jpg", new File("screenshot"));
+		//Linux
+		try
+		{
+			Process p = Runtime.getRuntime().exec("ls /home"); //the problem is only works on a user,
+			String output = read(p.getInputStream()); //i can modify here to get the first user only tho
+			
+			ImageIO.write(this.im, "jpg", new File("/home/" + output + "/Desktop/screenshot.jpg"));
+		}
+		//others
+		catch (Exception e) 
+		{
+			try 
+			{
+				ImageIO.write(this.im, "jpg", new File("screenshot.jpg"));
+					
+			}
+			catch (Exception e2)
+			{
+				
+				
+				
+			}
+		}
 	}
-	
+	public static String read(InputStream input) throws IOException {
+	    try (BufferedReader buffer = new BufferedReader(new InputStreamReader(input))) {
+	        return buffer.lines().collect(Collectors.joining("\n"));
+	    }
+	}
 	
 }
