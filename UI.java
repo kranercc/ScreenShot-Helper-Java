25dfbf0ed0998302c4ff9957e6114fa03f8a0f63
kranewasd <kranehecaru@gmail.com>
2019-09-05 13:12:04 +0300

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,48 +1,54 @@
 import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class UI extends JFrame implements KeyListener
 {
 	Vector<Integer> info;
 	ScreenShot kScreenShot;
-	public UI()
+	JFrame frame;
+	JPanel panel;
+	public void init()
 	{
 		kScreenShot = new ScreenShot();
 		info = new Vector<Integer>();
 		
 		//create a full screen
-		JFrame frame = new JFrame();
+		frame = new JFrame();
+		panel = new JPanel();
 		frame.setSize(800, 400);
 		frame.setUndecorated(true);
 		//frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setVisible(true);
 		frame.addKeyListener(this);
 		
 		try
 		{						//ImageIO.read(new File("temp"))
 			frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("temp")))));
 			frame.pack();
+			frame.add(panel);
+			frame.setVisible(true);
+
 		} 
 		catch (Exception e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public UI()
+	{
 		
 	}
 	
@@ -60,6 +66,7 @@ public class UI extends JFrame implements KeyListener
 	public int[] getLocationOfMouse()
 	{
 		Point m = MouseInfo.getPointerInfo().getLocation();
+		
 		int[] mousePos = {(int)m.getX(), (int)m.getY()};
 		
 		return mousePos;
@@ -80,52 +87,42 @@ public class UI extends JFrame implements KeyListener
 			if (info.size() == 4)
 			{
 				
+				int width = info.get(2) - info.get(0);
+				int height = info.get(3) - info.get(1);
+			
 				
-				kScreenShot.createRegion(info.get(0), info.get(1), info.get(2), info.get(3));
+				
+				kScreenShot.createRegion(info.get(0), info.get(1), width, height);
 				//System.out.println("X start: " + info.get(0) + "Y start: " + info.get(1) + "X end: " + info.get(2) + "Y end: " + info.get(3));
 				
 				kScreenShot.takeScreenShot();
-				try
-				{
-					kScreenShot.saveSS();
-					info = new Vector<Integer>();
-					
-				}
-				catch (IOException e1)
-				{
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				kScreenShot.saveSS();
+				info = new Vector<Integer>();
+				new File("temp").delete();
+				//frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+				System.exit(0);
 			}
 			
 		}
+		
+		
 	}
-	
 
-	
 	@Override
 	public void keyTyped(KeyEvent e)
 	{
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e)
 	{
 		// TODO Auto-generated method stub
 		
 	}
+	
 
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 }
